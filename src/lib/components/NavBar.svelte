f44376685668a02cb212e5f46e0410ea70cba318
Yehia Sharawy <65984199+YehiaSharawy@users.noreply.github.com>
2025-09-14 22:33:21 +0300

    +Fixed the NavBar Mobile Resposive Design +Changed old Favicon to the new version

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,10 +1,8 @@
 <script>
   import Button from "./Button.svelte";
-  import src from "$lib/assets/logos/skyxpert-team.jpg";
   import Logo from "$lib/assets/logos/skyxpert-short-logo.png";
   let isOpened = false;
   let navRoutes = [
-    // { label: 'Mission & Vision', href: '#mission' },
     { label: "Team", href: "#team" },
     { label: "Drones", href: "#drone" },
     { label: "Partnerships", href: "#sponsors" },
@@ -13,29 +11,26 @@
   ];
 </script>
 
-<!-- check if Navbar is opened or not -->
 <div class={isOpened ? "NavBar opened" : "NavBar"}>
   <div class="navContainer">
     <a href="/">
       <img src={Logo} alt="Logo" class="logo" />
     </a>
+
     <div class="navRightContainer">
       <div class="navButtons">
         {#each navRoutes as route}
           <a class="button" href={route.href}>{route.label}</a>
         {/each}
       </div>
+
       <Button
         class="menu-toggle"
         onclick={() => (isOpened = !isOpened)}
         expanded={isOpened}
       >
         {#if isOpened}
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+          <svg viewBox="0 0 24 24" fill="none">
             <path
               d="M18 6L6 18M6 6L18 18"
               stroke="currentColor"
@@ -45,11 +40,7 @@
             />
           </svg>
         {:else}
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+          <svg viewBox="0 0 24 24" fill="none">
             <path
               d="M3 12H21M3 6H21M3 18H21"
               stroke="currentColor"
@@ -67,13 +58,13 @@
 
 <style>
   .NavBar {
-    /*background-color: red;*/
     display: flex;
     justify-content: center;
     position: fixed;
     width: 100%;
     margin-top: 5vh;
     transition: background 0.3s;
+    z-index: 20;
 
     .navContainer {
       display: flex;
@@ -81,7 +72,6 @@
       align-items: center;
       position: fixed;
       width: 90%;
-      /*background-color: yellow;*/
 
       .navRightContainer {
         display: flex;
@@ -95,39 +85,39 @@
           height: 20px;
           margin-right: 0.3em;
         }
+      }
+    }
+    .navButtons {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      max-width: 650px;
+      /*background-color: magenta;*/
 
-        .navButtons {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
+      .button {
+        position: relative;
+        margin-right: 1em;
+        color: white;
+        font-family: "Bull", monospace;
+        font-variation-settings: "wght" 220;
+        font-size: 1.2em;
+
+        &::after {
+          content: "";
+          position: absolute;
+          bottom: -0.5em;
+          left: 0;
+          height: 2px;
           width: 100%;
-          max-width: 650px;
+          background-color: red;
+          transform: scaleX(0);
+          transform-origin: bottom;
+          transition: transform 0.1s ease-in-out;
+        }
 
-          .button {
-            position: relative;
-            margin-right: 1em;
-            color: white;
-            font-family: "Bull", monospace;
-            font-variation-settings: "wght" 220;
-            font-size: 1.2em;
-
-            &::after {
-              content: "";
-              position: absolute;
-              bottom: -0.5em;
-              left: 0;
-              height: 2px;
-              width: 100%;
-              background-color: red;
-              transform: scaleX(0);
-              transform-origin: bottom;
-              transition: transform 0.1s ease-in-out;
-            }
-
-            &:hover::after {
-              transform: scaleX(1);
-            }
-          }
+        &:hover::after {
+          transform: scaleX(1);
         }
       }
     }
@@ -168,32 +158,36 @@
         display: flex;
         align-items: center;
       }
+    }
+    .navButtons:nth-child(1) {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      justify-content: space-evenly;
+      height: 100%;
+      font-size: 2em;
 
-      .navButtons {
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        justify-content: space-evenly;
-        height: 100%;
+      .button {
+        margin: 0;
         font-size: 2em;
+        color: rgba(255, 255, 255, 0.6);
 
-        .button {
-          margin: 0;
-          font-size: 2em;
-          color: rgba(255, 255, 255, 0.6);
+        &::after {
+          bottom: -0.2em;
+        }
 
-          &::after {
-            bottom: -0.2em;
-          }
-
-          &:hover {
-            color: white;
-          }
+        &:hover {
+          color: white;
         }
       }
     }
   }
 
-  @media (min-width: 900px) {
+  @media (max-width: 900px) {
+    .NavBar {
+      .navButtons {
+        display: none;
+      }
+    }
   }
 </style>
